c6be76b8cfa35b055c076a283fc1c8aaf5386511
Tobias Thummerer <tobias.thummerer@uni-a.de>
2026-01-23 10:49:46 +0100

    initial files

---
+++ src/OptunaDashboard.jl
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2026 Julian Trommer, Valentin Höpfner, Andreas Hofman, Josef Kircher, Tobias Thummerer, and contributors
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
+module OptunaDashboard
+
+greet() = print("Hello World!")
+
+end # module OptunaDashboard
